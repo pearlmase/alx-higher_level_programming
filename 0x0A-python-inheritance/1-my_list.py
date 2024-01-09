5fe112f9baca31e4e97a89716dce041e3e56d99e
pearlmase <masentlepearl4@gmail.com>
2024-01-09 09:24:57 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+"""MyList module"""
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+
+class MyList(list):
+    """MyList class - Inherits from list"""
+    def print_sorted(self):
+        """Prints a sorted list"""
+        print(sorted(self))
